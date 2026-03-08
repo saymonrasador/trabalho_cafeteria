39bab6b0f71817f50d046d490404d92e14a81772
“Saymon <srrasador@ucs.br>
2026-03-08 12:52:32 -0300

    primeiro commit

---
+++ src/cafeteria/Cafeteria.java
@@ -0,0 +1,63 @@
+package cafeteria;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+// Deveria ter uma classe Pedido, Bebida e Cliente, usando interfaces para definir os comportamentos (Interface Segregation Principle (ISP))
+
+public class Cafeteria { // Não está usando interface
+	public void processarPedido(String tipoBebida, int quantidade, String tipoCliente) {
+		double precoBase = 0;
+
+		// Deveria existir uma classe Bebida e cada tipo de bebida deveria ser uma
+		// subclasse, usando polimorfismo para devolver o preço (Single Responsibility
+		// Principle (SRP) e levar em consideração o liskov Substitution Principle
+		// (LSP))
+		if (tipoBebida.equalsIgnoreCase("Cafe")) {
+			precoBase = 10.0;
+		} else if (tipoBebida.equalsIgnoreCase("Capuccino")) {
+			precoBase = 15.5;
+		} else if (tipoBebida.equalsIgnoreCase("Cha")) {
+			precoBase = 8.0;
+		}
+		// O cálculo do total do pedido deveria ser responsabilidade da classe Pedido, e
+		// a classe Cafeteria apenas retornaria seus valores (Single Responsibility
+		// Principle (SRP))
+		double total = precoBase * quantidade;
+		// Professor deveria ser uma subclasse de Cliente, usando polimorfismo para
+		// aplicar o desconto (Liskov Substitution Principle (LSP))
+		if (tipoCliente.equalsIgnoreCase("Professor")) {
+			total = total * 0.8; // 20% de desconto
+		}
+
+		// Não deveria ser de responsabilidade da classe Cafeteria interagir diretamente
+		// com o usuário, isso deveria ser responsabilidade de uma classe separada, e a
+		// classe cafeteria apenas retornaria seus valores (Single Responsibility
+		// Principle (SRP))
+		System.out.println("Total do pedido: R$ " + total);
+
+		// Não é responsabilidade da classe Cafeteria salvar o pedido em um arquivo,
+		// isso deveria ser responsabilidade de uma classe separada (Single
+		// Responsibility Principle (SRP))
+
+		try {
+			// Criar um método separado para salvar o pedido, seguindo o Single
+			// Responsibility Principle (SRP)
+			FileWriter writer = new FileWriter("log_pedidos.txt", true);
+			// Não pode instanciar o FileWriter diretamente aqui, deveria depender de uma
+			// interface para abstrair a escrita em arquivos (Dependency Inversion Principle
+			// (DIP))
+			writer.write("Pedido: " + tipoBebida + " | Total: " + total + "\n");
+			writer.close();
+			System.out.println("Pedido salvo no arquivo com sucesso.");
+		} catch (IOException e) {
+			System.out.println("Erro crítico no sistema de arquivos!");
+		}
+	}
+
+	public static void main(String[] args) {
+		Cafeteria sistema = new Cafeteria();
+		// Simulação de uso
+		sistema.processarPedido("Capuccino", 2, "Professor");
+	}
+}
---
+++ src/cafeteria/InterfaceTeste.java
@@ -0,0 +1,10 @@
+package cafeteria;
+
+public class InterfaceTeste {
+
+	public static void main(String[] args) {
+		Cafeteria sistema = new Cafeteria();
+		// Simulação de uso
+		sistema.processarPedido("Capuccino", 2, "Professor");	}
+
+}
